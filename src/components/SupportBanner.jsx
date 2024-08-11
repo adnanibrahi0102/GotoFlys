c836185e8f261990ae078331b5054f129b1e4028
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-08-11 17:25:21 +0530

    fixed phoneNumber

--- src/components/SupportBanner.jsx
+++ src/components/SupportBanner.jsx
@@ -70,7 +70,7 @@ const SupportBanner = () => {
           We’ll beat any online ticket price.
         </p>
         <a
-          href={`tel:+18663302527`}
+          href={`tel:${phoneNumber}`}
           className="col-span-1 md:col-span-2 lg:col-span-6 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-purple-800 bg-[linear-gradient(110deg,#6a0dad,45%,#dc143c,55%,#6a0dad)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-purple-50 w-full text-center"
         >
           <IoCall className="mr-3 text-xl font-bold mt-1" />
